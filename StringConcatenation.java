1cd50aaa993f792ad9c249bc7445c475158df599
chakradhar <chakradharghute@gamil.com>
2022-09-25 11:49:34 +0530

    Create StringConcatenation.java

---
+++ StringConcatenation.java
@@ -0,0 +1,9 @@
+class StringConcatenation {
+    public static void main(String args[]) {
+        /*
+         * By + (String concatenation) operator
+         */
+        String s1 = "Ramesh" + "Suresh";
+        System.out.println(s1);
+    }
+}
